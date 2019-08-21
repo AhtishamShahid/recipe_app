e58fd5003c0e0955dad8d54e428afa4b8375a089
Ahtisham.shahid <ahtisham@A002-00087.arbisoft.com>
2019-08-21 14:10:52 +0500

    fixed date error

--- recipe/models.py
+++ recipe/models.py
@@ -11,7 +11,7 @@ class Ingredients(models.Model):
     Ingredients model having m2m with recipe
     """
     title = models.CharField(max_length=30)
-    pub_date = models.DateTimeField(default=now())
+    pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -25,7 +25,7 @@ class Recipe(models.Model):
     description = models.CharField(max_length=30)
     directions = models.CharField(max_length=600)
     user = models.ForeignKey(User, related_name='recipe', on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(default=now())
+    pub_date = models.DateTimeField(auto_now=True)
     ingredients = models.ManyToManyField(Ingredients)
 
     def __str__(self):
